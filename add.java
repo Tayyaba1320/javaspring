0cf3c506e640ac2197dadd78200768e1c89e5f48
Tayyaba1320 <107556209+Tayyaba1320@users.noreply.github.com>
2022-06-16 18:08:55 +0530

    Update add.java

--- add.java
+++ add.java
@@ -5,6 +5,11 @@ spring Program
 
 class X1
 {
-        public static void main{
-        }
+        public static void main(String arg[])
+        {
+                a=10;
+                b=34;
+                c=a+b;
+                System.out.println(c);
        }
+}
